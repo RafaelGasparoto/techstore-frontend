ba7c660e883ca587f26cbd8990193dffedd60c9d
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-07-19 16:52:55 -0300

    Componente botão com ícone

---
+++ src/app/components/icon-button/icon-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'icon-button',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './icon-button.component.html',
+  styleUrl: './icon-button.component.scss'
+})
+export class IconButtonComponent {
+  @Input() icon: string = '';
+  @Input() color: string = '#2ba2da';
+
+}
