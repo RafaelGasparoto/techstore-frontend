09a012401afb86eb65a15c55423ecea67f935f20
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-08-01 22:33:09 -0300

    Calcular frete
    - Criado estrutura
    - Aplicado estilo

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from "../../components/button/button.component";
 import { CartOverviewComponent } from '../../components/overview-cart/cart-overview.component';
 import { CartItemComponent } from '../../components/cart-item/cart-item.component';
+import { InputComponent } from '../../components/input/input.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [ButtonComponent, CartOverviewComponent, CartItemComponent],
+  imports: [ButtonComponent, CartOverviewComponent, CartItemComponent, InputComponent],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.scss'
 })
 export class CartPageComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      cep: [''],
+    });
+  }
 
 }
