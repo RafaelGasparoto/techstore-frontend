54cf39052d0e86b6d67fbfbfeb6fe55cab266abd
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-08-01 21:34:26 -0300

    Design carrinho
    - Aplicado css em toda a página
    - Melhorias na estrutura de exibição

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -4,10 +4,12 @@ import { IconButtonComponent } from "../icon-button/icon-button.component";
 @Component({
   selector: 'cart-item',
   standalone: true,
-  imports: [IconButtonComponent, IconButtonComponent],
+  imports: [IconButtonComponent],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.scss'
 })
 export class CartItemComponent {
-
+  decrementItem(): void {}
+  incrementItem(): void {}
+  removeItem(): void {}
 }
